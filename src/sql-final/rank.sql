cab957e2c873c466b43156269ea6282ca2ada42e
mrquaketotheworld <newrushiflyer@gmail.com>
2023-04-28 20:19:49 +0300

    Add table rank

---
+++ src/sql-final/rank.sql
@@ -0,0 +1,7 @@
+CREATE TABLE rank (
+  id SERIAL PRIMARY KEY,
+  rank VARCHAR(255) NOT NULL UNIQUE, -- Pro
+  points INT NOT NULL UNIQUE, -- 32
+  color VARCHAR(100) NOT NULL UNIQUE, -- #ffffff
+  created_at timestamptz DEFAULT NOW(),
+);
