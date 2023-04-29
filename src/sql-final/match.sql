8f2f1bf97b5c3a51c3399c8063e7969443277a8c
mrquaketotheworld <newrushiflyer@gmail.com>
2023-04-29 11:57:36 +0300

    Fix type in match

--- src/sql-final/match.sql
+++ src/sql-final/match.sql
@@ -3,7 +3,7 @@ CREATE TABLE match (
   map VARCHAR(100) NOT NULL REFERENCES map(map),
   team1_score INT NOT NULL,
   team2_score INT NOT NULL,
-  team1 INT UNIQUE REFERENCES team(id),
-  team2 INT UNIQUE REFERENCES team(id),
+  team1 INT NOT NULL UNIQUE REFERENCES team(id),
+  team2 INT NOT NULL UNIQUE REFERENCES team(id),
   created_at timestamptz DEFAULT NOW()
 );
