2ec333623251097557bd97b45be4fbaf7ed70970
mrquaketotheworld <newrushiflyer@gmail.com>
2023-04-29 18:00:02 +0300

    Add insert map

---
+++ src/sql-final/insert/map.sql
@@ -0,0 +1,24 @@
+INSERT INTO map (map)
+VALUES
+('Nuke'),
+('Inferno'),
+('Mirage'),
+('Dust2'),
+('Tuscan'),
+('Train'),
+('Cache'),
+('Aztec'),
+('Cbble'),
+('CPLFire'),
+('CPLMill'),
+('Dust'),
+('Overpass'),
+('Prodigy'),
+('Vertigo'),
+('Assault'),
+('HEKrystal'),
+('IceWorld'),
+('Mansion'),
+('PoolDay'),
+('PoolDay2');
+
