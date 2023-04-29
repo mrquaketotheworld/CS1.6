2ec5c2a097e38a978cc3a17338a4b1d2c8ca73e5
mrquaketotheworld <newrushiflyer@gmail.com>
2023-04-29 12:23:13 +0300

    Rename player_team

--- src/sql-final/player_team.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE player_team (
-  id SERIAL PRIMARY KEY,
-  player_id VARCHAR(255) NOT NULL REFERENCES player(player_id),
-  team_id INT NOT NULL REFERENCES team(id),
-  server_id VARCHAR(255) NOT NULL REFERENCES server(server_id),
-  created_at timestamptz DEFAULT NOW(),
-);
