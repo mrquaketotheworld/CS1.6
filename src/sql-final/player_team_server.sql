a721798c452c946619d71718089db2c24fdae673
mrquaketotheworld <newrushiflyer@gmail.com>
2023-04-29 12:08:16 +0300

    Fix naming

---
+++ src/sql-final/player_team_server.sql
@@ -0,0 +1,7 @@
+CREATE TABLE player_team_server (
+  id SERIAL PRIMARY KEY,
+  player_id VARCHAR(255) NOT NULL REFERENCES player(player_id),
+  team_id INT NOT NULL REFERENCES team(id),
+  server_id VARCHAR(255) NOT NULL REFERENCES server(server_id),
+  created_at timestamptz DEFAULT NOW(),
+);
