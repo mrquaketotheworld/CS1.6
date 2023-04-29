59fd3743ef410730d826018a827aa62b4cb391f9
mrquaketotheworld <newrushiflyer@gmail.com>
2023-04-29 16:19:42 +0300

    Fix SQLs

--- src/sql-final/quote_author_check.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE quote_author_check (
-  author VARCHAR(255) PRIMARY KEY,
-  created_at timestamptz DEFAULT NOW() NOT NULL
-);
--- src/sql-final/rank.sql
+++ src/sql-final/rank.sql
@@ -1,6 +1,6 @@
 CREATE TABLE rank (
   rank VARCHAR(255) PRIMARY KEY, -- Pro
-  points INT NOT NULL UNIQUE, -- 32
-  color VARCHAR(100) NOT NULL UNIQUE, -- #ffffff
+  points INT PRIMARY KEY, -- 32
+  color VARCHAR(100) PRIMARY KEY, -- #ffffff
   created_at timestamptz DEFAULT NOW() NOT NULL
 );
