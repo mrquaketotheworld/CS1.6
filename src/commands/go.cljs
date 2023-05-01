70596ceda3162daceeeac1f21c42f5171bfcc217
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-01 18:31:03 +0300

    Add collector & fix compiler options

--- src/commands/go.cljs
+++ src/commands/go.cljs
@@ -5,12 +5,12 @@
             [db.connection :as db]
             [db.models.server :as server]
             [db.models.map-server :as map-server]))
-
+; TODO remove ^js/Object
 (def builder
   (.. (discord/SlashCommandBuilder.)
       (setName "go")
       (setDescription "Run map poll!")
-      (addStringOption (fn [^js/Object option]
+      (addStringOption (fn [option]
                          (.. option
                              (setName "mapmode")
                              (setDescription "Choose map mode")
@@ -48,17 +48,21 @@
        :components (create-buttons maps)})
 
 (defn convert-users-to-string [users]
-  (reduce (fn [acc ^js/Object user]
+  (reduce (fn [acc user]
             (str acc "<@" (.. user -user -id) ">")) "" users))
+
+(defn handle-collector-event [event]
+  (println event))
 
 (def MAP-QUESTION ":triangular_flag_on_post: **VOTE FOR THE MAP PLEASE**, **1 MINUTE TO VOTE**")
 
 (def state (atom {}))
 
-(defn interact! [^js/Object interaction]
+(defn interact! [interaction]
   (let [option (or (.. interaction -options (getString "mapmode")) "main")
         server-id (.. interaction -guild -id)
         server-name (.. interaction -guild -name)]
+    (println "/go INTERACTION" (js/Date.))
     (go (try
           (let [client (<p! (.connect db/pool))
               server-with-maps (.-rows (<p! (map-server/check-server-with-maps-exists server-id)))]
@@ -76,8 +80,7 @@
             (let [maps
                   (format-maps (js->clj (.-rows (<p! (map-server/select-maps server-id option)))))
                   guild-id (.. interaction -guild -id)
-                  interaction-id (.-id interaction)
-                  ]
+                  interaction-id (.-id interaction)]
               (if (.. interaction -member -voice -channel)
                 (do
                   (when-not (contains? @state guild-id)
@@ -97,12 +100,27 @@
                   (<p! (.reply interaction (create-reply MAP-QUESTION maps)))
                   (let [users-in-voice
                         (convert-users-to-string (.from js/Array
-                                (.. interaction -member -voice -channel -members values)))]
-
-                    (<p! (.followUp interaction users-in-voice)))
-                  ) ; if user in voice, do
+                                (.. interaction -member -voice -channel -members values)))
+                        channel-id (.. interaction -channel -id)]
+                    (<p! (.followUp interaction users-in-voice))
+                    (when-not (get-in @state [guild-id :collectors channel-id])
+                      (swap! state assoc-in [guild-id :collectors channel-id]
+                             (.. interaction
+                                 -channel
+                                 (createMessageComponentCollector #js
+                                                              {:componentType
+                                                             (.-Button discord/ComponentType)}))))
+                    (let [collector (get-in @state [guild-id :collectors channel-id])]
+                      (.on collector "collect" handle-collector-event)))) ; if user in voice, do
                   (<p! (.reply
                          interaction #js {:content (str "You're not in the voice channel, "
                                                         (.. interaction -member -user -username))
                                           :ephemeral true})))))
           (catch js/Error e (println e))))))
+
+
+
+
+(def x (atom {:cars {:a "Toyota" :b "BMW"}}))
+
+(swap! x assoc-in [:cars :a] "Suzuki")
