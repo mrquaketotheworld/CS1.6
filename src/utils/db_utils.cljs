b68e02cbe638e384abd5e04038cf3e8d60854c7f
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-08 00:29:14 +0300

    Add db_utils, refactor get

---
+++ src/utils/db_utils.cljs
@@ -0,0 +1,7 @@
+(ns utils.db-utils)
+
+(defn get-formatted-rows [query-result]
+  (js->clj (.-rows query-result)))
+
+(defn get-first-formatted-row [query-result]
+  (first (get-formatted-rows query-result)))
