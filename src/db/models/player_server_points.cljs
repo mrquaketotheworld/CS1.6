4e0b52999d76ffcc2639f8ef8d987fdd0c93edae
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-08 17:24:23 +0300

    Fix server_id

--- src/db/models/player_server_points.cljs
+++ src/db/models/player_server_points.cljs
@@ -2,16 +2,16 @@
   (:require [db.connection :as db]))
 
 (defn select-player-by-server
-  ([player-id server_id] (select-player-by-server db/pool player-id server_id))
-  ([client player-id server_id]
+  ([player-id server-id] (select-player-by-server db/pool player-id server-id))
+  ([client player-id server-id]
   (.query client
   "SELECT player_id, points FROM player_server_points WHERE player_id = $1 AND server_id = $2"
-    #js [player-id server_id])))
+    #js [player-id server-id])))
 
-(defn select-players-points [client players-ids server_id]
+(defn select-players-points [client players-ids server-id]
   (.query client
  "SELECT player_id, points FROM player_server_points WHERE player_id = ANY ($1) AND server_id = $2"
-    #js [players-ids server_id]))
+    #js [players-ids server-id]))
 
 (defn select-player-rating [player-id server-id]
   (.query db/pool
@@ -19,14 +19,14 @@
                   FROM player_server_points WHERE server_id = $1) AS sub WHERE sub.player_id = $2"
     #js [player-id server-id]))
 
-(defn insert-player [client player-id server_id]
+(defn insert-player [client player-id server-id]
   (.query client
   "INSERT INTO player_server_points (player_id, server_id) VALUES ($1, $2)"
-    #js [player-id server_id]))
+    #js [player-id server-id]))
 
-(defn update-player-points [client player-id server_id points]
+(defn update-player-points [client player-id server-id points]
   (.query client
   "UPDATE player_server_points SET points = points + $1 WHERE player_id = $2 AND server_id = $3"
-    #js [points player-id server_id]))
+    #js [points player-id server-id]))
 
 
