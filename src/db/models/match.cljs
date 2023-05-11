aa0c8be84b14a1f7fbbaa13188ad98fbe783fb6b
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-11 13:08:22 +0300

    Add id in insert-match

--- src/db/models/match.cljs
+++ src/db/models/match.cljs
@@ -3,7 +3,7 @@
 
 (defn insert-match [client map-name team1-score team2-score team1-id team2-id]
   (.query client (str "INSERT INTO match (map, team1_score, team2_score, team1, team2)"
-                      "VALUES ($1, $2, $3, $4, $5)"
+                      "VALUES ($1, $2, $3, $4, $5) RETURNING id"
                 ) #js [map-name team1-score team2-score team1-id team2-id]))
 
 (defn select-team1-wins [team-ids]
